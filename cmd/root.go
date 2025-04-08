76ae2c475a0c52c5ed69c344449967c3aeb7d502
boqiu <82121246@qq.com>
2025-04-08 14:48:53 +0800

    Init cmd and config

---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"github.com/Conflux-Chain/go-conflux-util/config"
+	"github.com/Conflux-Chain/go-conflux-util/log"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "confura-data-cache",
+	Short: "Start confura data cache service",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return cmd.Help()
+	},
+}
+
+func init() {
+	config.MustInit("CDC")
+
+	log.BindFlags(rootCmd)
+}
+
+// Execute is the command line entrypoint.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		logrus.WithError(err).Fatal("Failed to execute command")
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Conflux-Chain/confura-data-cache/cmd"
+
+func main() {
+	cmd.Execute()
+}
