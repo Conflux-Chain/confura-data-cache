4a9e1d6d2f39fe965a6270787dc3c5d35cf87d9f
Conflux <wanliqunn@163.com>
2025-04-09 17:49:22 +0800

    add interface check

--- store/store.go
+++ store/store.go
@@ -3,7 +3,13 @@ package store
 import (
 	"io"
 
+	"github.com/Conflux-Chain/confura-data-cache/store/leveldb"
 	"github.com/Conflux-Chain/confura-data-cache/types"
+)
+
+var (
+	_ Store[types.EthBlockData] = (*leveldb.Store[types.EthBlockData])(nil)
+	_ Store[types.CfxBlockData] = (*leveldb.Store[types.CfxBlockData])(nil)
 )
 
 // Store interface to manage block data in persistent storage.
