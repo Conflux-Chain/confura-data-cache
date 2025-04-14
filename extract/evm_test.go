d753c8e0078778c9f27cba9fa1aa1f93b3bf4cfc
Conflux <wanliqunn@163.com>
2025-04-14 11:52:51 +0800

    improve testing

--- extract/evm_test.go
+++ extract/evm_test.go
@@ -7,23 +7,9 @@ import (
 	"testing"
 
 	"github.com/Conflux-Chain/confura-data-cache/extract"
-	"github.com/Conflux-Chain/confura-data-cache/store/leveldb"
 	"github.com/mcuadros/go-defaults"
 	"github.com/stretchr/testify/assert"
 )
-
-func createTestStore(t *testing.T) (*leveldb.Store, func()) {
-	path, err := os.MkdirTemp("", "confura-data-cache-")
-	assert.Nil(t, err)
-
-	store, err := leveldb.NewStore(path)
-	assert.Nil(t, err)
-
-	return store, func() {
-		store.Close()
-		os.RemoveAll(path)
-	}
-}
 
 func TestEvmExtract(t *testing.T) {
 	endpoints := os.Getenv("TEST_EVM_RPC_ENDPOINTS")
@@ -32,12 +18,8 @@ func TestEvmExtract(t *testing.T) {
 		return
 	}
 
-	store, close := createTestStore(t)
-	defer close()
-
 	conf := extract.Config{
-		StartBlockNumber: store.NextBlockNumber(),
-		RpcEndpoints:     strings.Split(endpoints, ","),
+		RpcEndpoints: strings.Split(endpoints, ","),
 	}
 	defaults.SetDefaults(&conf)
 
