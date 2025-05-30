3ec2cfb82e985b7517eec6150f5e75efe0393e5d
boqiu <82121246@qq.com>
2025-05-30 12:48:58 +0800

    Supports call timeout for gRPC client

--- rpc/client_proto.go
+++ rpc/client_proto.go
@@ -2,6 +2,7 @@ package rpc
 
 import (
 	"context"
+	"time"
 
 	pb "github.com/Conflux-Chain/confura-data-cache/rpc/proto"
 	"github.com/Conflux-Chain/confura-data-cache/types"
@@ -14,17 +15,26 @@ type ClientProto struct {
 	inner pb.EthClient
 
 	url string
+
+	timeout time.Duration
 }
 
-func NewClientProto(url string) (*ClientProto, error) {
+func NewClientProto(url string, timeout ...time.Duration) (*ClientProto, error) {
 	conn, err := grpc.Dial(url)
 	if err != nil {
 		return nil, err
 	}
 
-	client := pb.NewEthClient(conn)
+	callTimeout := 3 * time.Second
+	if len(timeout) > 0 {
+		callTimeout = timeout[0]
+	}
 
-	return &ClientProto{client, url}, nil
+	return &ClientProto{
+		inner:   pb.NewEthClient(conn),
+		url:     url,
+		timeout: callTimeout,
+	}, nil
 }
 
 func (c *ClientProto) String() string {
@@ -32,7 +42,10 @@ func (c *ClientProto) String() string {
 }
 
 func (c *ClientProto) GetBlock(bhon types.BlockHashOrNumber, isFull bool) (types.Lazy[*ethTypes.Block], error) {
-	resp, err := c.inner.GetBlock(context.Background(), &pb.GetBlockRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	resp, err := c.inner.GetBlock(ctx, &pb.GetBlockRequest{
 		BlockId: pb.NewBlockId(bhon),
 		IsFull:  isFull,
 	})
@@ -41,7 +54,10 @@ func (c *ClientProto) GetBlock(bhon types.BlockHashOrNumber, isFull bool) (types
 }
 
 func (c *ClientProto) GetBlockTransactionCount(bhon types.BlockHashOrNumber) (int64, error) {
-	resp, err := c.inner.GetBlockTransactionCount(context.Background(), pb.NewBlockId(bhon))
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	resp, err := c.inner.GetBlockTransactionCount(ctx, pb.NewBlockId(bhon))
 	if err != nil {
 		return 0, err
 	}
@@ -50,13 +66,19 @@ func (c *ClientProto) GetBlockTransactionCount(bhon types.BlockHashOrNumber) (in
 }
 
 func (c *ClientProto) GetTransactionByHash(txHash common.Hash) (types.Lazy[*ethTypes.TransactionDetail], error) {
-	resp, err := c.inner.GetTransactionByHash(context.Background(), pb.NewTransactionId(txHash))
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	resp, err := c.inner.GetTransactionByHash(ctx, pb.NewTransactionId(txHash))
 
 	return pb.ResponseToLazy[*ethTypes.TransactionDetail](resp, err)
 }
 
 func (c *ClientProto) GetTransactionByIndex(bhon types.BlockHashOrNumber, txIndex uint32) (types.Lazy[*ethTypes.TransactionDetail], error) {
-	resp, err := c.inner.GetTransactionByIndex(context.Background(), &pb.GetTransactionByIndexRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	resp, err := c.inner.GetTransactionByIndex(ctx, &pb.GetTransactionByIndexRequest{
 		BlockId: pb.NewBlockId(bhon),
 		TxIndex: txIndex,
 	})
@@ -65,25 +87,37 @@ func (c *ClientProto) GetTransactionByIndex(bhon types.BlockHashOrNumber, txInde
 }
 
 func (c *ClientProto) GetTransactionReceipt(txHash common.Hash) (types.Lazy[*ethTypes.Receipt], error) {
-	resp, err := c.inner.GetTransactionReceipt(context.Background(), pb.NewTransactionId(txHash))
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	resp, err := c.inner.GetTransactionReceipt(ctx, pb.NewTransactionId(txHash))
 
 	return pb.ResponseToLazy[*ethTypes.Receipt](resp, err)
 }
 
 func (c *ClientProto) GetBlockReceipts(bhon types.BlockHashOrNumber) (types.Lazy[[]ethTypes.Receipt], error) {
-	resp, err := c.inner.GetBlockReceipts(context.Background(), pb.NewBlockId(bhon))
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	resp, err := c.inner.GetBlockReceipts(ctx, pb.NewBlockId(bhon))
 
 	return pb.ResponseToLazy[[]ethTypes.Receipt](resp, err)
 }
 
 func (c *ClientProto) GetTransactionTraces(txHash common.Hash) (types.Lazy[[]ethTypes.LocalizedTrace], error) {
-	resp, err := c.inner.GetTransactionTraces(context.Background(), pb.NewTransactionId(txHash))
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	resp, err := c.inner.GetTransactionTraces(ctx, pb.NewTransactionId(txHash))
 
 	return pb.ResponseToLazy[[]ethTypes.LocalizedTrace](resp, err)
 }
 
 func (c *ClientProto) GetBlockTraces(bhon types.BlockHashOrNumber) (types.Lazy[[]ethTypes.LocalizedTrace], error) {
-	resp, err := c.inner.GetBlockTraces(context.Background(), pb.NewBlockId(bhon))
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	resp, err := c.inner.GetBlockTraces(ctx, pb.NewBlockId(bhon))
 
 	return pb.ResponseToLazy[[]ethTypes.LocalizedTrace](resp, err)
 }
